4e5ba2363943fb1606b18fdaf2e2f14d410caffb
gonnatinfo2000 <58569152+gonnatinfo2000@users.noreply.github.com>
2022-01-17 14:51:33 +0000

    adição da função soma e subtração

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
+	public int soma(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int subtracao(int num1, int num2) {
+		return num1 - num2;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!"); // Display the string.
+		System.out.println("Hello World!"); // Display the string;
 	}
 	
 }
