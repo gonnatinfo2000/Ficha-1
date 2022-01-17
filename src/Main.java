aba7146b55e3a44d71a67386794fb81d55ad9a15
gonnatinfo2000 <58569152+gonnatinfo2000@users.noreply.github.com>
2022-01-17 15:04:04 +0000

    adição da função operaçãoComplexa

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
 
 	public int multiplicacao (int num1, int num2) {
-		return num1*num2;
+		return num1 * num2;
 	}
+	
 	public int soma(int num1, int num2) {
 		return num1 + num2;
 	}
 	
 	public int subtracao(int num1, int num2) {
 		return num1 - num2;
+	}
+	
+	public int operacaoComplexa(int num1, int num2) {
+		return multiplicacao (num1, num2) + subtracao(num1, num2);
 	}
 	
 	public static void main(String[] args) {
