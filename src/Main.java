d4e9d2cce3e514bc2bc10cf437ac077bd0fb2e2a
MargaridaD <97111622+MargaridaD@users.noreply.github.com>
2022-01-17 14:54:57 +0000

    adição da função multiplicação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+	public int multiplicacao (int num1, int num2) {
+		return num1*num2;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string.
 	}
