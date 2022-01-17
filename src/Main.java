e988316b9d037aed6cf4ecab93bc4dad554768f9
MargaridaD <97111622+MargaridaD@users.noreply.github.com>
2022-01-17 15:05:55 +0000

    adição da função operaçãoComplexa

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,11 @@ public class Main {
 		return num1 - num2;
 	}
 	
+	
+	public int operaçãoComplexa(int num1,int num2) {
+		return soma(num1, num2)*subtracao(num1,num2);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string;
 	}
